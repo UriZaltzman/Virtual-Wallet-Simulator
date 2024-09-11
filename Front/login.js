8c0b3371450909cc187985ab8c2af6e040425b13
PabloVives30 <48116412@est.ort.edu.ar>
2024-09-11 17:47:49 -0300

    registro y login les falta nada mas el fetch (js)

--- Front/login.js
+++ Front/login.js
@@ -1,13 +1,11 @@
 document.getElementById('button').addEventListener('click',async ()=> {
-    const username = document.getElementById('login-username').value;
-    const lastname = document.getElementById('login-lastname').value;
     const email = document.getElementById('login-email').value;
     const password = document.getElementById('login-password').value;
 
     const response = await fetch('https://db-projecto.vercel.app/login', {
         method: 'POST',
         
-        body: JSON.stringify({ username, lastname, email, password })
+        body: JSON.stringify({email, password })
     });
 
     const data = await response.json();
@@ -15,16 +13,6 @@ document.getElementById('button').addEventListener('click',async ()=> {
     
     if (data.success) {
         window.location.href = '/validate';
-    }
-});
-
-// Mostrar/ocultar contraseña
-document.getElementById('show-password').addEventListener('change', function() {
-    const passwordInput = document.getElementById('login-password');
-    if (this.checked) {
-        passwordInput.type = 'text';
-    } else {
-        passwordInput.type = 'password';
     }
 });
 
