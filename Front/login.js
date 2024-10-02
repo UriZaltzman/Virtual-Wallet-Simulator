0649cc531590f03c46d416d24a33e5b44809b00e
PabloVives30 <48116412@est.ort.edu.ar>
2024-10-02 11:26:41 -0300

    some changes in login

--- Front/login.js
+++ Front/login.js
@@ -2,17 +2,42 @@ document.getElementById('button').addEventListener('click',async ()=> {
     const email = document.getElementById('login-email').value;
     const password = document.getElementById('login-password').value;
 
-    const response = await fetch('https://db-projecto.vercel.app/login', {
+    infoPersona={
+        mail:email,
+        contasena:password
+    }
+
+    const response = await fetch('http://localhost:3000/login', { //https://db-projecto.vercel.app
         method: 'POST',
-        
-        body: JSON.stringify({email, password })
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(infoPersona),
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("Error en el login");
+        }
+        return response.json();
+    })
+    .then(data => {
+        alert("Login exitoso");
+        return window.location.href = "homePage.html";
+    })
+    .catch(error => {
+        console.log(error);
+        alert("Hubo un problema con el login");
     });
 
+    
+
+
+/* 
     const data = await response.json();
     alert(data);
     
     if (data.success) {
         window.location.href = '/validate';
-    }
+    } */
 });
 
