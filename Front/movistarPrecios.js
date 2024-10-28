2d967f5f22f340b9f9793ba0e8a6a9d73fb85536
PabloVives30 <48116412@est.ort.edu.ar>
2024-10-28 00:22:00 -0300

    phonePage done
    without fetch*

---
+++ Front/movistarPrecios.js
@@ -0,0 +1,49 @@
+const btnPlan1 = document.getElementById("btnPlan1");
+const btnPlan2 = document.getElementById("btnPlan2");
+const btnPlan3 = document.getElementById("btnPlan3");
+const btnPlan4 = document.getElementById("btnPlan4");
+
+btnPlan1.addEventListener("click", function(){
+
+    if(confirm("¿Estás seguro de este plan?")){
+        window.location.href = "movistarProgress.html";
+    }
+    else{
+        alert("Elige otro plan");
+        return;
+    }
+}) 
+
+btnPlan2.addEventListener("click", function(){
+
+    if(confirm("¿Estás seguro de este plan?")){
+        window.location.href = "movistarProgress.html";
+    }
+    else{
+        alert("Elige otro plan");
+        return;
+    }
+}) 
+
+btnPlan3.addEventListener("click", function(){
+
+    if(confirm("¿Estás seguro de este plan?")){
+        window.location.href = "movistarProgress.html";
+    }
+    else{
+        alert("Elige otro plan");
+        return;
+    }
+}) 
+
+btnPlan4.addEventListener("click", function(){
+
+    if(confirm("¿Estás seguro de este plan?")){
+        window.location.href = "movistarProgress.html";
+    }
+    else{
+        alert("Elige otro plan");
+        return;
+    }
+}) 
+
