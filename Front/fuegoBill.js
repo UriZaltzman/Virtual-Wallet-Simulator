976477a69cda58cb07700790949b87737f0749d0
Pepe08tn <48521743@est.ort.edu.ar>
2024-11-11 13:36:52 -0300

    Bills Gas

--- Front/fuegoBill.js
+++ Front/fuegoBill.js
@@ -50,4 +50,94 @@ btnPagar.addEventListener("click", function() {
     }, 40);
 });
 
-document.addEventListener("DOMContentLoaded", function() {alert("3")});
+document.addEventListener("DOMContentLoaded", function () {
+    const impuestoCardTemplate = document.querySelector("[data-impuestos-template]");
+    const impuestoCardContainer = document.querySelector("[data-impuestos-cards-container]");
+
+    let impuestos = [];
+
+    function renderImpuestosCards(impuestosToRender) {
+        impuestoCardContainer.innerHTML = "";
+        impuestosToRender.forEach(impuesto => {
+
+            const cardButton = document.createElement("button");
+            cardButton.classList.add("card-button");
+            cardButton.type = "button";
+
+            const card = impuestoCardTemplate.content.cloneNode(true).children[0];
+            const tipo = card.querySelector("[data-tipo]");
+            const saldo = card.querySelector("[data-saldo]");
+
+            tipo.textContent = impuesto.tipo;
+            saldo.textContent = impuesto.saldo;
+
+            cardButton.append(card);
+            impuestoCardContainer.append(cardButton);
+
+            cardButton.addEventListener("click", () => {
+                if (confirm(`¿Estás seguro que quieres pagar este impuesto por $${impuesto.saldo}?`)) {
+                    /*sessionStorage.setItem("id_impuesto_pagar", impuesto.id_impuesto);
+                    window.location.href = "pagarImpuesto.html";*/
+                    const token = localStorage.getItem('authToken');  // Asegúrate de que 'authToken' contiene el token
+    
+                    fetch("https://db-projecto.vercel.app/pagarImpuesto", {
+                        method: 'POST',
+                        headers: {
+                            "Authorization": `Bearer ${token}`,  // Envía el token en el formato correcto
+                            "Content-Type": "application/json"
+                        },
+                        body: JSON.stringify({ id_impuesto: impuesto.id_impuesto, saldo: impuesto.saldo }),
+                    })
+                        .then(response => {
+                            if (!response.ok) {
+                                throw new Error("Error al pagar impuesto");
+                            }
+                            CargarImpuestosPendientes();
+                        })
+                }
+            });
+
+            //user.element = cardButton;  
+        });
+    }
+
+    function CargarImpuestosPendientes() {
+        const infoTipoImpuesto = {
+            tipo: 3 // TIPO 2 electricidad - 1 agua - 3 fuego
+        };
+    
+        const token = localStorage.getItem('authToken');  // Asegúrate de que 'authToken' contiene el token
+    
+        fetch("https://db-projecto.vercel.app/verimpuestos", {
+            method: 'POST',
+            headers: {
+                "Authorization": `Bearer ${token}`,  // Envía el token en el formato correcto
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(infoTipoImpuesto),
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error("Error al obtener la información de los usuarios");
+                }
+                return response.json();
+            })
+            .then(data => {
+                if (data.success && data.results) {
+                    impuestos = data.results.map(impuesto => ({
+                        id_impuesto: impuesto.id,
+                        saldo: impuesto.saldo,
+                        tipo: "GAS"
+                    }));
+    
+                    renderImpuestosCards(impuestos);
+                } else {
+                    console.error("Error en la respuesta:", data.message);
+                    impuestoCardContainer.innerHTML = "<p>No se encontró un impuesto.</p>";
+                }
+            })
+            .catch(error => console.error("Error en la solicitud:", error));
+    }
+
+    CargarImpuestosPendientes();
+});
