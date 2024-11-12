897c8106b3fc6b440cdecf78f44a9fe41d758458
PabloVives30 <48116412@est.ort.edu.ar>
2024-11-12 08:20:23 -0300

    datosCuenta Page design done

--- Front/datosCuenta.js
+++ Front/datosCuenta.js
@@ -1,4 +1,24 @@
 document.addEventListener("DOMContentLoaded", function() {
+    
+    const iconMenu = document.getElementById("iconMenu");
+    if(iconMenu){
+        iconMenu.addEventListener("click", () => {
+            window.location.href = "homePage.html";
+        })
+    }
+
+    const bellIcon = document.getElementById("bellIcon");
+    bellIcon.addEventListener("click", () => {
+        alert("Proximamente...");
+        return;
+    })
+
+    const helpIcon = document.getElementById("helpIcon");
+    helpIcon.addEventListener("click", () => {
+        alert("Proximamente...");
+        return;
+    })
+
     const token = localStorage.getItem('authToken'); 
     
     const mailElement = document.getElementById("mail");
