2a4cf70ad32caf2d1e74c65e97143a8d001142fb
PabloVives30 <48116412@est.ort.edu.ar>
2024-11-11 21:32:45 -0300

    elegirTarjeta page design done

--- Front/elegirTarjeta.js
+++ Front/elegirTarjeta.js
@@ -1,25 +1,48 @@
-const tarjetasList = document.getElementById("tarjetasList");
-const continuarBtn = document.getElementById("continuar");
+document.addEventListener("DOMContentLoaded", () => {
+    const tarjetasList = document.getElementById("tarjetasList");
+    const continuarBtn = document.getElementById("continuar");
+    const tarjetasSube = JSON.parse(localStorage.getItem("tarjetasSube")) || [];
 
-const tarjetasSube = JSON.parse(localStorage.getItem("tarjetasSube")) || [];
 
-
-tarjetasSube.forEach((tarjeta, index) => {
-    const tarjetaBtn = document.createElement("button");
-    tarjetaBtn.textContent = "Tarjeta SUBE: " + tarjeta;
-    tarjetaBtn.addEventListener("click", function() {
-        seleccionarTarjeta(tarjeta);
+    tarjetasSube.forEach((tarjeta, index) => {
+        const tarjetaBtn = document.createElement("button");
+        tarjetaBtn.textContent = "Tarjeta SUBE: " + tarjeta;
+        tarjetaBtn.addEventListener("click", function() {
+            seleccionarTarjeta(tarjeta);
+        });
+        tarjetasList.appendChild(tarjetaBtn);
     });
-    tarjetasList.appendChild(tarjetaBtn);
-});
 
-function seleccionarTarjeta(tarjeta) {
+    function seleccionarTarjeta(tarjeta) {
+        localStorage.setItem("tarjetaSeleccionada", tarjeta);
+        alert("Has seleccionado la tarjeta: " + tarjeta);
+        continuarBtn.disabled = false;
+    }
 
-    localStorage.setItem("tarjetaSeleccionada", tarjeta);
-    alert("Has seleccionado la tarjeta: " + tarjeta);
-    continuarBtn.disabled = false;
-}
+    continuarBtn.addEventListener("click", function() {
+        window.location.href = "cargarSube.html"; 
+    });
 
-continuarBtn.addEventListener("click", function() {
-    window.location.href = "cargarSube.html"; 
-});
+    const iconMenu = document.getElementById("iconMenu");
+    if (iconMenu) {
+        iconMenu.addEventListener("click", () => {
+            window.location.href = "homePage.html";
+    });
+    }
+
+    const bellIcon = document.getElementById("bellIcon");
+    if(bellIcon){
+        bellIcon.addEventListener("click", () => {
+            alert("Proximamente...");
+            return;
+        })
+    }
+
+    const helpIcon = document.getElementById("helpIcon");
+    if(helpIcon){
+        helpIcon.addEventListener("click", () => {
+            alert("Proximamente...");
+            return;
+        })
+    }
+})
