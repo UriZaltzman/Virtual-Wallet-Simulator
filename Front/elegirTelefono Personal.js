3c31486fac72d9be91e1f3b81a140697054657b3
PabloVives30 <48116412@est.ort.edu.ar>
2024-11-13 00:14:13 -0300

    PERSONAL design done

--- Front/elegirTelefono Personal.js
+++ Front/elegirTelefono Personal.js
@@ -47,3 +47,23 @@ function seleccionarTelefono(telefono) {
 continuarBtn.addEventListener("click", function() {
     window.location.href = "personalPrecios.html";
 });
+
+const iconMenu = document.getElementById("iconMenu");
+if (iconMenu) {
+    iconMenu.addEventListener("click", () => {
+        window.location.href = "homePage.html";
+    });
+}
+
+const bellIcon = document.getElementById("bellIcon");
+    bellIcon.addEventListener("click", () => {
+        alert("Proximamente...");
+        return;
+})
+
+
+const helpIcon = document.getElementById("helpIcon");
+    helpIcon.addEventListener("click", () => {
+        alert("Proximamente...");
+        return;
+})
--- Front/personalProgress.js
+++ Front/personalProgress.js
@@ -20,3 +20,23 @@ btnPagar.addEventListener("click", function() {
         }
     }, 40);
 });
+
+const iconMenu = document.getElementById("iconMenu");
+if (iconMenu) {
+    iconMenu.addEventListener("click", () => {
+        window.location.href = "homePage.html";
+    });
+}
+
+const bellIcon = document.getElementById("bellIcon");
+    bellIcon.addEventListener("click", () => {
+        alert("Proximamente...");
+        return;
+})
+
+
+const helpIcon = document.getElementById("helpIcon");
+    helpIcon.addEventListener("click", () => {
+        alert("Proximamente...");
+        return;
+})
